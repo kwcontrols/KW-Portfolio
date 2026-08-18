a8b3899ca959f54664818fe91d6288d54253eedb
kwcontrols <teck3618@gmail.com>
2026-08-17 17:36:54 -0700

    Restore portfolio GA4 measurement ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 import "./portal-auth.css";
 import "./statistics-layout-fix.css";
 
-const GA_MEASUREMENT_ID = "G-BH5VVGY72M";
+const GA_MEASUREMENT_ID = "G-76RBFH4SD1";
 
 export async function generateMetadata(): Promise<Metadata> {
   const incomingHeaders = await headers();
